3d30754b1e58de917f024f1c0026bb64578f20e7
Jennifer Switzer <jfs@mit.edu>
2020-09-17 02:15:59 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn with_name(_: TokenStream, item: TokenStream) -> TokenStream {
+    let mut input = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let fn_name = input.ident.to_string();
+    let const_decl = quote::quote! {
+        const THIS_FN: &str = #fn_name;
+    };
+
+    input.block.stmts.insert(0, syn::parse(const_decl.into()).unwrap());
+
+    let output = quote::quote! {
+        #input
+    };
+
+    output.into()
+}
